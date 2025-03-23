c539e4827217fb9433958dca4a7e83bd382e88fc
ballisticdicts <jaydabholkar08@gmail.com>
2025-03-23 00:00:18 -0700

    Orginized and created info page elements

--- GuiPrep.java
+++ GuiPrep.java
@@ -52,7 +52,7 @@ public class GuiPrep {
 
         JButton infoButton = new JButton("i");
         infoButton.setFont(new Font("Arial", Font.BOLD, 16));
-        infoButton.setPreferredSize(new Dimension(40, 20));
+        infoButton.setPreferredSize(new Dimension(40, 10));
         infoButton.addActionListener(e -> {
             CardLayout cl = (CardLayout)(cards.getLayout());
             cl.show(cards, INFO_PANEL);
@@ -144,8 +144,10 @@ public class GuiPrep {
         mainPanel.add(showHeightButton);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 20)));
         mainPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        //calc display area
         mainPanel.add(showCalcButton);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        // calc display label jsut concatonates strings
         mainPanel.add(calcDisplayLabel);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 20)));
   
@@ -155,29 +157,87 @@ public class GuiPrep {
     }
 
     private JPanel createInfoPanel() {
+        //main info panel
         JPanel infoPanel = new JPanel();
         infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
         infoPanel.setBackground(new Color(0, 0, 50));
 
-        JLabel infoTitle = new JLabel("BMI Info");
-        infoTitle.setFont(new Font("Arial", Font.BOLD, 24));
+        // elements that go onto info title panel
+        //titile for info page
+        JLabel infoTitle = new JLabel("BMI Info And Concept Idea");
+        infoTitle.setFont(new Font("Arial", Font.BOLD, 32));
         infoTitle.setForeground(Color.WHITE);
         infoTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        JButton backButton = new JButton("Back to Calculator");
-        backButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //page swap button
+        JButton backButton = new JButton("i");
+        backButton.setFont(new Font("Arial", Font.BOLD, 16));
+        backButton.setPreferredSize(new Dimension(40, 10));
         backButton.addActionListener(e -> { //lambda fuction
             CardLayout cl = (CardLayout)(cards.getLayout());
             cl.show(cards, MAIN_PANEL);
             });
+        //actually adding these elements onto info title panel
+        //info title panel
+        JPanel infoTitlePanel = new JPanel();
+        infoTitlePanel.setLayout(new BorderLayout());
+        infoTitlePanel.setBackground(new Color(0, 0, 50));
+        infoTitlePanel.setMaximumSize(new Dimension(500, 50));
+
+        infoTitlePanel.add(infoTitle, BorderLayout.CENTER);
+        infoTitlePanel.add(backButton, BorderLayout.EAST);    
+
+        // elements that go onto sub title panel
+        // concept title
+        JLabel conceptTitle = new JLabel("concept");
+        conceptTitle.setFont(new Font("Arial", Font.BOLD, 24));
+        conceptTitle.setForeground(Color.WHITE);
+        conceptTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+        // What is BMI title
+        JLabel bmiTitle = new JLabel("BMI");
+        bmiTitle.setFont(new Font("Arial", Font.BOLD, 24));
+        bmiTitle.setForeground(Color.WHITE);
+        bmiTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //creation and adding stuff into subtitile panel
+        JPanel subTitlePanel = new JPanel();
+        subTitlePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 160, 0));
+        subTitlePanel.setBackground(new Color(0, 0, 50));
+        subTitlePanel.add(bmiTitle);
+        subTitlePanel.add(conceptTitle);
+        // stuff that'll go in sub text panel
+        //concept and idea paragraph
+        JLabel conceptText = new JLabel("<html><p style='width: 100px;'>"
+        + "BMI is a measure of body fat based on height and weight."
+        + " It applies to adult men and women."
+        + "</p></html>");
+        conceptText.setFont(new Font("Arial", Font.BOLD, 14));
+        conceptText.setForeground(Color.WHITE);
+        conceptText.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 
+        JLabel bmiText = new JLabel("<html><p style='width: 100px;'>"
+        + "BMI is a measure of body fat based on height and weight."
+        + " It applies to adult men and women."
+        + "</p></html>");
+        bmiText.setFont(new Font("Arial", Font.BOLD, 14));
+        bmiText.setForeground(Color.WHITE);
+        bmiText.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        //sub text panel creation and additions
+        JPanel subTextPanel = new JPanel();
+        subTextPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 0));
+        subTextPanel.setBackground(new Color(0, 0, 50));
+        subTextPanel.add(bmiText);
+        subTextPanel.add(conceptText);
+
+        // panel additions
+
         infoPanel.add(Box.createRigidArea(new Dimension(0, 20)));
-        infoPanel.add(infoTitle);
-        infoPanel.add(Box.createRigidArea(new Dimension(0, 20)));
-        infoPanel.add(backButton);
-        infoPanel.add(Box.createRigidArea(new Dimension(0, 20)));
-        
+        infoPanel.add(infoTitlePanel);
+        infoPanel.add(Box.createRigidArea(new Dimension(0, 60)));
+        infoPanel.add(subTitlePanel);
+        infoPanel.add(subTextPanel);
+
+
         
 
 
